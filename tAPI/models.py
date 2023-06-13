4b6c1b0ad75a14a5ced29d19a911389c2e0bb067
shublynx <shubham683075@gmail.com>
2023-06-13 18:11:19 +0530

    adding serializers and defining GET and POST method.

--- tAPI/models.py
+++ tAPI/models.py
@@ -3,6 +3,9 @@ from django.db import models
 # Create your models here.
 class Product(models.Model):
 
+    def __str__(self):
+        return self.title
+
     title = models.CharField(max_length=250)
     content = models.TextField()
     price = models.DecimalField(max_digits=15,decimal_places=2,default=99.99)
--- tAPI/views.py
+++ tAPI/views.py
@@ -1,3 +1,23 @@
 from django.shortcuts import render
+from .models import Product
+from django.http.response import JsonResponse
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+from rest_framework import viewsets
+from .serializers import ProductSerializer
 
-# Create your views here.
+@api_view(['GET','PUT','POST'])
+def index(request):
+    if request.method == 'GET':
+
+        quesryset = Product.objects.all()
+        serializer = ProductSerializer(quesryset,many=True)
+        return Response(serializer.data)
+
+    elif request.method == 'POST':
+
+        posted_data = request.data    # data posted
+        serializer = ProductSerializer(data=posted_data)
+        if serializer.is_valid():
+            serializer.save()
+        return Response(serializer.data)
