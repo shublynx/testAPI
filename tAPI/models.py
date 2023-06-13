ed83346a28664285693f32b2b7f442cfc23b24bd
shublynx <shubham683075@gmail.com>
2023-06-13 16:27:32 +0530

    product model creation

--- tAPI/models.py
+++ tAPI/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
 # Create your models here.
+class Product(models.Model):
+
+    title = models.CharField(max_length=250)
+    content = models.TextField()
+    price = models.DecimalField(max_digits=15,decimal_places=2,default=99.99)
